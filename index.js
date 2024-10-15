56e8a591b9424bcf0fa42635aa498b7abe8484e0
Soham Daga <soham.daga22@gmail.com>
2024-10-15 12:13:10 -0700

    Triggering workflow

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
